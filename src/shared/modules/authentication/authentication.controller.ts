986907be24c988c00864849eccf34b840d13510e
akihito7 <akihitopro7@gmail.com>
2025-01-25 17:55:37 -0300

    feat : add auth module and types kanel and validation pipes

--- src/shared/modules/authentication/authentication.controller.ts
+++ src/shared/modules/authentication/authentication.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { SignupRequestDto } from './dtos/request/signup-request-dto';
+import { SignlnRequestDto } from './dtos/request/signln-request.dto';
+import { SignlnResponseDto } from './dtos/response/signln-response-dto';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Post('signup')
+  @HttpCode(201)
+  async signup(@Body() body: SignupRequestDto) {
+    return this.authenticationService.signup(body);
+  }
+
+  @Post('signln')
+  async signln(@Body() body: SignlnRequestDto) : Promise<SignlnResponseDto>{
+    return this.authenticationService.signln(body);
+  }
 }
