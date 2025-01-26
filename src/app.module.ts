d25a3ff22eaf451f0fea3c7df1539f580f9aa9a7
akihito7 <akihitopro7@gmail.com>
2025-01-26 18:51:22 -0300

    feat : add employee module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthenticationModule } from './shared/modules/authentication/authentication.module';
 import { ScheduleModule } from './modules/schedule/schedule.module';
 import { OfferingsModule } from './modules/offerings/offerings.module';
+import { EmployeeModule } from './modules/employee/employee.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { OfferingsModule } from './modules/offerings/offerings.module';
     AuthenticationModule,
     ScheduleModule,
     OfferingsModule,
+    EmployeeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
