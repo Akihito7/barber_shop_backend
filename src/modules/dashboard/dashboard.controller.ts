4557ce1615b4b140e8280382da1b2d2f32a7fa2a
akihito7 <akihitopro7@gmail.com>
2025-02-27 20:34:00 -0300

    feat : add type query and verification to date in dashboard controller

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -1,32 +1,34 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { DashboardService } from './dashboard.service';
+import { IDate } from './dtos/request/date.dto';
 
 @Controller('dashboard')
+@UseGuards(AuthGuard)
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
   @Get('daily-revenue')
-  async getDailyRevenue(@Query() query) {
+  async getDailyRevenue(@Query() query: IDate) {
     return this.dashboardService.getDailyRevenue(query.date);
   }
 
   @Get('weekly-revenue')
-  async getWeeklyRevenue(@Query() query) {
+  async getWeeklyRevenue(@Query() query: IDate) {
     return this.dashboardService.getWeeklyRevenue(query.date);
   }
 
   @Get('monthly-revenue')
-  async getMonthlyRevenue(@Query() query) {
+  async getMonthlyRevenue(@Query() query: IDate) {
     return this.dashboardService.getMonthlyRevenue(query.date);
   }
 
   @Get('top-sellings')
-  async getTopSellingServices(@Query() query) {
+  async getTopSellingServices(@Query() query: IDate) {
     return this.dashboardService.getTopSellingServices(query.date);
   }
 
   @Get('by-semester')
-  async getRevenueBySemester(@Query() query) {
+  async getRevenueBySemester(@Query() query: IDate) {
     return this.dashboardService.getRevenueBySemester(query.date);
   }
 }
---
+++ src/modules/dashboard/dtos/request/date.dto.ts
@@ -0,0 +1,3 @@
+export class IDate {
+  date: string;
+}
